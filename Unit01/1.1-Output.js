8cf9c636086b5e70175e06b3448283c7ad6b9efa
LiamRodrigs <liam.rodrigs@stu.ocsb.ca>
2024-03-20 21:36:50 -0400

    New Commit

---
+++ Unit01/1.1-Output.js
@@ -0,0 +1,15 @@
+console.log("Apple Inc.");
+console.log("One Apple Park Way");
+console.log("Cupertino, CA");
+console.log("95014");
+console.log("+1 408-996-1010");
+
+console.log(""); // Output a blank line
+console.log(""); // Output another blank line
+
+console.log(`Microsoft Corporation\nOne Microsoft Way\nRedmond, WA\n98052\n+1 425-882-8080`);
+
+console.log(""); // Output a blank line
+console.log(""); // Output another blank line
+
+console.log(`x\t o\t x\no\t x\t o\nx\t o\t x`);
