14d5e2a365057a95341e21f8f6af1b220be0633d
LiamRodrigs <liam.rodrigs@stu.ocsb.ca>
2024-04-04 13:52:29 -0400

    c

--- Unit01/1.2 - Variables.js
+++ Unit01/1.2 - Variables.js
@@ -1,5 +1,10 @@
 let length = 10;
 let width = 5;
 let area =  length*width;
-console.log("50")
-console.log("Area:50")
+console.log("50");
+console.log("Area:50");
+
+let first_name = Santa;
+let last_name = Claws;
+let last_name = Claus;
+console.log("I still believe in Santa Claus.");
