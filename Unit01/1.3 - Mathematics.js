258fdf82af4a553cf2d8907f74870ae4464e4ca3
LiamRodrigs <liam.rodrigs@stu.ocsb.ca>
2024-04-04 19:37:52 -0400

    b

--- Unit01/1.3 - Mathematics.js
+++ Unit01/1.3 - Mathematics.js
@@ -1,4 +1,36 @@
-let celsisus = 100;
-let farenheight = 0;
-let addition = celsius + 2;
+// Declare variables and set initial values
+let celsius = 100;
+let fahrenheit = 0;
 
+// Add 2 to celsius
+celsius += 2;
+
+// Convert Celsius to Fahrenheit using the formula: (°C × 9/5) + 32
+fahrenheit = (celsius * 9/5) + 32;
+
+// Output the value of fahrenheit to the console
+console.log(fahrenheit);
+
+// Set the variable fahrenheit to 77
+fahrenheit = 77;
+
+// Convert Fahrenheit to Celsius using the formula: (°F - 32) * 5/9
+celsius = (fahrenheit - 32) * 5/9;
+
+// Output the current value of celsius to the console
+console.log(celsius);
+
+// Print the conversion 
+console.log(fahrenheit + " degrees Fahrenheit is " + celsius + " degrees Celsius.");
+
+// Define the coordinates of points A and B
+let x1 = -4;
+let y1 = 5;
+let x2 = 6;
+let y2 = 0;
+
+// Calculate the slope
+let m = (y2 - y1) / (x2 - x1);
+
+// Print the value of m to the console
+console.log("The slope of the line passing through points A(-4, 5) and B(6, 0) is: " + m);
