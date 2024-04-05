449f534f36ebf57a0c28fc7f5d08afb4a79c5d5f
LiamRodrigs <liam.rodrigs@stu.ocsb.ca>
2024-04-05 14:14:12 -0400

    b

--- Unit01/1.4 - User Input.js
+++ Unit01/1.4 - User Input.js
@@ -1,6 +1,22 @@
 const prompt = require('prompt-sync')();
-let age = prompt("How old are you? ");
-console.log("You said you are", age, "years old.");
 
-let user = prompt("1.4 - User Input");
-age = prompt("How old is your dog? ");
+console.log("1.4 - User Input");
+
+// Asking for the year of birth
+let yearOfBirth = prompt("What year were you born? >");
+console.log("You entered " + yearOfBirth);
+let currentYear = new Date().getFullYear();
+let age = currentYear - yearOfBirth;
+console.log("You are likely " + age + " years old");
+
+// Asking for the name
+let name = prompt("What is your name? >");
+console.log("Hi, " + name + ". You were born in " + yearOfBirth + " and you are likely " + age + " years old.");
+
+// Printing the length of the name
+console.log("Your name is " + name.length + " letters long.");
+
+// Converting temperature from Fahrenheit to Celsius
+let fahrenheit = prompt("Enter temperature in Fahrenheit: ");
+let celsius = (fahrenheit - 32) * (5 / 9);
+console.log("Temperature in Celsius: " + celsius.toFixed(2));
