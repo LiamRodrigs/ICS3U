b38e58fd3cc41fc55db3a4fa5bef329c4750370e
Liam Rodrigs <liam.rodrigs@stu.ocsb.ca>
2024-04-15 14:25:17 -0400

    new file

---
+++ Unit01/1.6 - Logical Operators & Modulus.js
@@ -0,0 +1,24 @@
+// Asking user if they want to play a game
+prompt("Do you want to play a game? (y/n):");
+
+// User enters a number 
+prompt("Enter a number")
+
+// the user gave us a number and it's stored in "user_number"
+if (user_number > 10 && user_number < 50) {
+    console.log("Your next number should be indeed greater than 50");
+}
+
+// User enters a second number 
+prompt("Enter a second number")
+
+// the user gave us a second number and it's stored in "user_number"
+if (user_number == even) {
+console.log("Your next number should be indeed an odd value");
+(user_number == odd) } {
+console.log("Your next number should be indeed an even value");
+}
+
+// User enters a third and final number 
+prompt("Enter a third and your final number")
+
