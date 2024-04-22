113b7ad077984645e5b02c0c40f0996b8b5b3e85
Liam Rodrigs <liam.rodrigs@stu.ocsb.ca>
2024-04-22 14:19:31 -0400

    New file of 2.2

---
+++ Unit01/Unit02/2.2 - Return Statements.js
@@ -0,0 +1,29 @@
+// Creating a function of a weekday and depending upon a weekend day, it will therefore return true or false
+function is_it_the_weekend(day) {
+        if (day = 1 || 2) {
+          return true;
+        } else (day = 3 && 4) 
+          return false;}
+
+// Creating a new function of a certain value and seeing if it is divisble by 9 and also even additionally
+function evNineIble(value) {
+    if (value = x/9 + even) {
+        return true;
+    }   else (value != x/9 + even)
+        return false;}
+
+// Creating a function defining the area of a circle and 
+function area_circle(r) {
+    if (rgiven = x) {
+        return radiusgiven;
+    }   else (A = 3.14*x)
+        return areaofacircle;} 
+
+// Creating a function defining the area of a circle and 
+function area_label(r, h) {
+    if (rgiven = x) {
+        return radiusgiven;
+    if (hgiven = y) {
+    }   return heightgiven;
+    }   else (A = 3.14*x)
+        return areaofacircle;} 
