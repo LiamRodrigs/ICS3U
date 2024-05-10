8786057a393a5e16daa14e1e1170bcde608d8d7b
Liam Rodrigs <liam.rodrigs@stu.ocsb.ca>
2024-05-10 14:26:01 -0400

    File Started and completed the other

---
+++ Unit01/Unit02/Unit03/3.5 - Nested Loops.js
@@ -0,0 +1,13 @@
+if (something == true) {
+    if (some_other_thing == true) {
+      if (x % 2 == 0) {
+      }
+    }
+  }
+
+for (let outer_loop = 0; outer_loop < 10; outer_loop++) {
+    
+    for (let inner_loop = 9; inner_loop > 0; inner_loop--) { 
+    }
+  }
+
---
+++ Unit01/Unit02/Unit03/3.6 - Arrays.js
@@ -0,0 +1,7 @@
+let somenumbers = [5,8,9,20,-29,20,5,0.5,1,2.5]
+// creating a random array of number that include some of different variations such as intergers, decimals, whole values etc.
+
+print_array(ArrayBuffer)
+// causing the interfacre to buffer the array
+
+print_array(arr)
